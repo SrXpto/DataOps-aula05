7523220316300530ebe7ed1172d804e18f290b03
André Batista <andre.c.batista8@gmail.com>
2025-01-22 19:39:13 +0000

    changes made on main.py

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ import matplotlib.pyplot as plt
 data = {'Nome': ['Alice', 'Bob'], 'Idade': [25, 30]}
 df = pd.DataFrame(data)
 print(df)
-plt.plot([1, 2, 3], [4, 5, 6])
+plt.plot([1, 2, 3,4], [4, 5, 6,7])
 plt.show()
